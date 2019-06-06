1e0b77db80fa71b299130610ec56e50e6e4a7029
unknown <ich.bin.abe@gmail.com>
2019-06-06 16:25:49 -0400

    Kel haha

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,7 @@
 <section class="grid-container grid-x grid-padding-y align-top">
-    
+    <section class="cell grid-x align-center">
+        <img class="cell shrink"
+            src="https://media.licdn.com/dms/image/C5603AQGBSCzzXjjenw/profile-displayphoto-shrink_200_200/0?e=1565222400&v=beta&t=1hxJ_9M_EQ3j9SEaDrF-Vnv5vXM4UA3yJBuXFXRCr9I" />
+    </section>
 </section>
+
